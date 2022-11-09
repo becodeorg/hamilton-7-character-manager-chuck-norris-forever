492b61a090f93dd5317f0be36cddbe34688a2360
Vittoria-P <vittoriapellizzari.pro@gmail.com>
2022-11-09 16:22:19 +0100

    Add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   content: ["./src/index.html" , "./src/character.html" , "./src/create.html"],
   theme: {
-    fontFamily:{
-      'h1': ['League Spartan, sans-serif'] ,
+    extends: {
+      fontFamily: {
+        'western': ['"League Spartan"', 'sans-serif'],
+      },
     },
+    
   },
   plugins: [],
 }
