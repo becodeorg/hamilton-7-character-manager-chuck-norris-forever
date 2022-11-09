2a377b4d5e8ed7d5c6cba629a642e443507afec2
Vittoria-P <vittoriapellizzari.pro@gmail.com>
2022-11-09 09:56:27 +0100

    Add info

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["src/index.html"],
+  content: ["./src/index.html" , "./src/character.html" , "./src/create.html"],
   theme: {
     fontFamily:{
       'h1': ['League Spartan, sans-serif'] ,
